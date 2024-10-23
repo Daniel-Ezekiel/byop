ba21e37258b7258102e99d1b01c4a182a3f1f91a
Daniel Ezekiel <dbezekiel77@gmail.com>
2024-10-24 00:40:42 +0100

    feat: responsiveness for dashboard hero section complete

--- src/app/[authUser]/dashboard/page.tsx
+++ src/app/[authUser]/dashboard/page.tsx
@@ -7,12 +7,12 @@ function Dashboard() {
   return (
     <MainLayout isDashboardPage={true}>
       <section className='py-12'>
-        <div className='w-full flex items-center justify-between rounded-xl p-4 py-6 bg-gradient-linear-300 lg:px-10 lg:pr-16 xl:w-[75rem] 2xl:w-[90rem]'>
-          <div className='w-1/2'>
+        <div className='w-full flex items-center justify-between rounded-xl p-4 py-6 bg-gradient-linear-300 md:px-10 lg:gap-10 lg:px-16 lg:pr-16 xl:w-[75rem] 2xl:w-[90rem]'>
+          <div className='sm:w-1/2'>
             <h1 className='w-[max-content] font-[family-name:var(--font-general-sans-semibold)] text-2xl md:text-3xl lg:text-4xl xl:text-5xl'>
               Good morning Daniel!
             </h1>
-            <p className='py-3 max-w-[26rem] lg:text-lg'>
+            <p className='py-3 md:max-w-[26rem] lg:text-lg'>
               Welcome to the dashboard! Here you can manage your portfolio and
               track your information.
               <br />
@@ -26,7 +26,7 @@ function Dashboard() {
           <Image
             src={ImgBuildPortfolio}
             alt='Build your own portfolio illustration'
-            className='w-full md:w-60 md:place-self-end lg:w-96'
+            className='hidden sm:block sm:w-60 md:place-self-end lg:w-96'
           />
         </div>
       </section>
